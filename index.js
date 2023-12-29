263a0e75fdb676e0661b723bf591e317ccedf905
Jermaine Dafoe <etesfaye@hotmail.com>
2023-12-29 22:43:00 +0300

    add request handler

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require('http');
 const sleep = require('then-sleep');
-const { serve } = require('micro');
+const { send, sendError, serve } = require('micro');
 
 const server = new http.Server(
   serve(async (req, res) => {
     await sleep(500);
-    return 'Hello world';
+    try {
+      send(res, 200, await requestHandler(req, res));
+    } catch (err) {
+      sendError(req, res, err);
+    }
   }),
 );
 
-server.listen(8888, () => console.log('Micro is Listening on localhost:8888'));
+server.listen(8888, () => console.log('Listening on port 8888'));
+
+async function requestHandler(req, res) {
+
+  console.log(req);
+}
